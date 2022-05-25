39109b1aab56dd32ee6b59b611754fd3f736f55f
Frederick Wells <pandabrand@gmail.com>
2022-05-25 13:21:31 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [msg, setMsg] = React.useState("click the button")
+  const handler = () =>
+    fetch("https://icanhazdadjoke.com/", { headers: { accept: "Accept: application/json" } })
+      .then((x) => x.json())
+      .then(({ msg }) => setMsg(msg))
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>message: {msg}</p>
+        <button onClick={handler}> click meeee</button>
       </header>
     </div>
-  );
+  )
 }
-
 export default App;
